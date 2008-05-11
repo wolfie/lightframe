79ea94e447f7bf3b8d281b8da07407efa8271963
Henrik Paul <henrik.paul@gmail.com>
2008-05-12 00:33:57 +0300

    Added filters "capitalize" and "capitalizefirst"

--- lib/template/filters.php
+++ lib/template/filters.php
@@ -7,6 +7,14 @@ function lf_filter_lowercase($string, $arg) {
 	return strtolower($string);
 }
 
+function lf_filter_capitalize($string, $arg) {
+	return ucwords($string);
+}
+
+function lf_filter_capitalisefirst($string, $arg) {
+	return ucfirst($string);
+}
+
 function lf_filter_default($string, $arg) {
 	if (!$string)
 		return $arg;
