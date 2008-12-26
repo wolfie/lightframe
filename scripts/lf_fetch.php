0016628bf5534b66878d09964a19d2382899a48f
Henrik Paul <henrik.paul@gmail.com>
2008-12-27 00:37:58 +0200

    Added the Apache License 2.0 header to each source file

--- scripts/lf_fetch.php
+++ scripts/lf_fetch.php
@@ -1,5 +1,21 @@
 #!/usr/bin/php
 <?php
+/*
+ * Copyright 2008 LightFrame Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Fetch a LightFrame copy from the web.
  * 
@@ -19,7 +35,6 @@
  * 
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License v2.0
  * @author Henrik Paul
- * 
  */
 
 // TODO: figure out a distribution structure
